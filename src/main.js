1d06ab9f6a69b5cdb073c781b90a2f60e474bc4e
王佩馨 <lovv840629y@gmail.com>
2020-07-24 16:35:18 +0800

    fix:  bootstrap 沒有動態效果

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'bootstrap';
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
