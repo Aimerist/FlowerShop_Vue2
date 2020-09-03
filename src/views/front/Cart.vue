5a6c6a00573ce263fb4f5fe69b59eedf0ee5bfe4
王佩馨 <lovv840629y@gmail.com>
2020-09-03 16:30:36 +0800

    style: 修改 樣式 in 購物清單頁|前台

--- src/views/front/Cart.vue
+++ src/views/front/Cart.vue
@@ -1,63 +1,109 @@
 <template>
-  <div>
+  <div class="container gap-setting">
     <div class="vld-parent">
       <loading :active.sync="isLoading"></loading>
     </div>
-    <div class="h4 text-center mb-3">購 物 車 清 單</div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>縮圖</th>
-          <th>品名</th>
-          <th>數量</th>
-          <th>小計</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="cart in Carts.carts" :key="cart.id">
-          <td class="thumbnail"
-            :style="{backgroundImage:`url(${cart.product.imageUrl})`}"></td>
-          <td>{{ cart.product.title }}</td>
-          <td>{{ cart.qty }} / {{ cart.product.unit }}</td>
-          <td>{{ cart.total | currency }}</td>
-          <td>
-            <a href="#" class="text-danger" @click.prevent="removeProductToCart(cart.id)">
-              <i class="fas fa-trash-alt"></i>
-            </a>
-          </td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colspan="3" class="text-right">總計</td>
-          <td class="text-right"> {{Carts.total | currency }}</td>
-        </tr>
-        <tr v-if="Carts.final_total !== Carts.total">
-          <td colspan="3" class="text-right text-success">折扣價</td>
-          <td class="text-right text-success">{{ Carts.final_total | currency }}</td>
-        </tr>
-      </tfoot>
-    </table>
-    <div class="input-group mb-3 input-group-sm">
-      <input type="text" class="form-control"
-        v-model="couponCode" placeholder="請輸入優惠碼">
-      <div class="input-group-append">
-        <button class="btn btn-outline-secondary" type="button"
-          @click="addCouponCode">
-          套用優惠碼
-        </button>
+    <!-- 流程 -->
+    <ul class="mb-4 step">
+      <li class="active">
+        <strong>STEP</strong>
+        <h3><strong>01</strong></h3>
+        <p class="h6">確認購物清單</p>
+      </li>
+      <li class="bg-light">
+        <strong>STEP</strong>
+        <h3><strong>02</strong></h3>
+        <p class="h6">填寫訂購資料</p>
+      </li>
+      <li class="bg-light">
+        <strong>STEP</strong>
+        <h3><strong>03</strong></h3>
+        <p class="h6">付款/完成訂單</p>
+      </li>
+    </ul>
+    <div class="row colorE">
+      <!-- 購物車清單-->
+      <div class="col-md-7">
+        <!-- <div class="jumbotron text-center bg-brownlight rounded-0">
+          <div class="h4 mb-5">購物車內無商品</div>
+          <router-link class="btn btn-lg btn-goshoping py-1 mt-4 rounded-0"
+            :to="{ name: 'Products'}" >
+            繼續購物</router-link>
+        </div> -->
+        <div class="p-3 bg-brownlight mb-4 md-dis-none">
+          <h4 class="text-center mb-0">購物車清單</h4>
+        </div>
+        <div class="cart-body" style="">
+          <div v-for="cart in Carts.carts" :key="cart.id">
+            <div class="d-flex align-items-center">
+              <img class="img-fluid mr-4" width="100" height="100"
+                :src="`${cart.product.imageUrl}`" alt="">
+              <div>
+                <h4>{{ cart.product.title }}
+                  <span class="text-success f-size75 mx-2"
+                    v-if="cart.total!==cart.final_total">(已套用優惠券)</span>
+                </h4>
+                <div clASS="d-flex h6 mb-0 text-gray">
+                  <div class="mr-4">{{ cart.product.price | currency }}</div>
+                  <span class="mr-4">x</span>
+                  <div class="mr-4">{{ cart.qty }} {{ cart.product.unit }}</div>
+                </div>
+              </div>
+              <div class="ml-auto h4 text-right">
+                <del class="f-size75 ml-5 text-gray"
+                  v-if="cart.total!==cart.final_total">
+                  {{ cart.total | currency }}</del>
+                <h4 class="ml-auto text-important font-weight-bold">
+                  {{ cart.final_total | currency }}</h4>
+              </div>
+              <button class="btn btn-outline-danger mr-4 ml-auto"
+                @click.prevent="removeProductToCart(cart.id)">
+                <i class="fas fa-trash-alt"></i>
+              </button>
+            </div>
+            <hr>
+          </div>
+        </div>
+        <router-link class="btn-block btn-back py-1 my-4 text-dark md-dis-none"
+          :to="{ name: 'Products'}">
+          <i class="fas  fa-arrow-left"></i>
+          繼續購物</router-link>
       </div>
-    </div>
-    <div class="mb-4 stepBtn">
-      <router-link to="/products" class="btn btn-secondary">
-        <i class="fas fa-arrow-left"></i>
-        繼續購買
-      </router-link>
-      <router-link to="/consumerform" class="btn btn-danger d-inline-block">
-        下一步，填寫訂購資料
-        <i class="fas fa-arrow-right"></i>
-      </router-link>
+      <!-- 購物車合計-折價券-->
+      <div class="col-md-5 my-md-0 my-2">
+        <div class="border p-3 shadow">
+          <span class="badge badge-pill badge-danger float-right bg-primary">3</span>
+          <div class="h5 text-center border-bottom pb-3">購 物 車 合 計</div>
+          <div class="d-flex px-3 py-2">
+            <h5>總計</h5>
+            <span class="ml-auto h5">{{ Carts.total | currency }}</span>
+          </div>
+          <div class="d-flex px-3 py-2"
+            v-if="Carts.total!==Carts.final_total">
+            <h6 clas="mt-2">折扣價</h6>
+            <span class="text-success f-size75 mx-2">(已套用優惠券)</span>
+            <h3 class="ml-auto text-success">{{ Carts.final_total | currency }}</h3>
+          </div>
+          <div class="input-group px-3">
+            <input type="text" class="form-control f-size75 py-3"
+              v-model="couponCode" placeholder="請輸入優惠碼">
+            <div class="input-group-append">
+              <button class="btn btn-sm btn-outline-gray" type="button"
+                @click="addCouponCode">套用優惠碼</button>
+            </div>
+          </div>
+          <small class="ml-auto py-1 d-block mb-3 px-3 text-gray">
+            <span>現在輸入OPEN50OFF即可享有折扣價喔!</span>
+          </small>
+          <div class="p-2">
+            <router-link :to="{ name: 'ConsumerForm' }"
+              class="btn btn-block btn-submit py-3 h5 rounded-0">
+              送出訂單
+              <i class="fas  fa-arrow-right"></i>
+            </router-link>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -114,11 +160,61 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
 .thumbnail {
   margin: auto;
   background-size: cover;
   width: 80px;
   height: 80px;
+}
+
+.customerorder-title {
+  @media (max-width:767px) {
+    display: none;
+  }
+}
+
+.step {
+  display: flex;
+  justify-content: center;
+  padding: 0;
+  text-align: center;
+  color: #6c757d;
+  li {
+    padding: 0.25rem 3rem;
+    @media (max-width:767px) {
+      display: none;
+    }
+    &:not(:last-child) {
+      margin-right: 0.25rem;
+    }
+  }
+  li.active {
+    padding: 0.25rem 3rem;
+    background-color: #EEDEA6;
+    color: #6B460F;
+    @media (max-width:767px) {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      background-color: #efe5dc;
+      color: #343a40;
+    }
+  }
+}
+
+.cart-body {
+  @media (min-width:768px) {
+    min-height: 210px
+  }
+}
+
+.btn-goshoping {
+  color: #efe5dc;
+  background-color: #B69418;
+  &:hover {
+    color: #efe5dc;
+    background-color: #D1A813;
+  }
 }
 </style>
