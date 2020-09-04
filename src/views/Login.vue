3ab0c06021eb31b776cca7a27a04e16d6b518f2e
王佩馨 <lovv840629y@gmail.com>
2020-09-04 12:17:22 +0800

    style: 修改 樣式 in 登入頁|前台

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,26 @@
 <template>
-  <div>
+  <div class="container gap-setting">
     <form class="form-signin" @submit.prevent="signin">
       <img class="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg"
         alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <h1 class="h3 mb-3 font-weight-bold text-brown text-center">管理者登入</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control"
         placeholder="Email address" required
         v-model="user.username">
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control"
+      <input type="password" id="inputPassword" class="form-control mb-3"
         placeholder="Password" required
         v-model="user.password">
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
+      <button class="btn btn-lg btn-primary btn-block text-white"
+        type="submit">登入</button>
+      <div class="btn-block text-center mt-4">
+        <router-link class="link-line text-brown font-weight-bold"
+          :to="{ name: 'Products' }">
+          <i class="fas  fa-arrow-left"></i>
+          回到 Flower Shop 購物去</router-link>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
+      <p class="mt-1 text-brown text-center">&copy; 2020</p>
     </form>
   </div>
 </template>
