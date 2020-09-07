4ff2f45f326ff076f866d1ea8d30911d3fc49ed8
王佩馨 <lovv840629y@gmail.com>
2020-09-07 14:18:12 +0800

    style: 新增 購物車 in 頁首|元件

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,10 +26,57 @@
             <li class="nav-item ml-auto">
               <!-- 購物車 -->
               <div class="btn-group">
-                <button type="button" class="btn text-dark" data-toggle="dropdown">
+                <a type="button" class="btn text-dark" data-toggle="dropdown">
                   <i class="fas fa-shopping-cart fa-lg"></i>
-                  <span class="badge badge-pill badge-danger">3</span>
-                </button>
+                  <span class="badge badge-pill badge-danger">{{ Carts.length }}</span>
+                </a>
+                <div class="dropdown-menu shadow">
+                  <div class="p-2 px-sm-3">
+                    <h5 class="text-center">購物車</h5>
+                    <table class="table mb-2 table-hover" style="min-width:270px">
+                      <tbody>
+                        <tr class="cursor-pointer"
+                          v-for="cart in Carts" :key="cart.id"
+                          @click="productLink(cart.product_id)">
+                          <td class="align-middle px-2">
+                            <a class="btn-del text-danger"
+                              @click.stop.prevent="removeCartItem(cart.id)">
+                              <i class="fas fa-trash-alt"></i>
+                            </a>
+                          </td>
+                          <td class="align-middle px-1">
+                            <div class="bg-cover"
+                              :style="`background-image:url(${ cart.product.imageUrl });`"></div>
+                          </td>
+                          <td class="py-1 px-0">
+                            <div class="align-middle px-0 f-size75">
+                              <div class="text-dark">{{ cart.product.title }}</div>
+                              <div class="text-secondary">
+                                {{ cart.qty }}/{{ cart.product.unit }}</div>
+                            </div>
+                          </td>
+                          <td class="text-right align-middle px-1 text-success font-weight-bold">
+                            {{ cart.total | currency }}</td>
+                        </tr>
+                        <tr>
+                          <td class="text-center p-0 m-0" v-if="Carts.length===0">
+                            <div class="text-center bg-brownlight m-0 p-4">
+                              <div>購物車內沒有任何商品唷!!</div>
+                              <router-link :to="{ name: 'Products'}"
+                                class="btn btn-md btn-primary py-1 mt-3 h5 text-white">
+                                前往購物</router-link>
+                            </div>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <router-link class="btn btn-block btn-important text-white"
+                      :to="{ name: 'Cart' }"
+                      v-if="Carts.length !== 0">
+                      <i class="fas fa-cart-arrow-down"></i>
+                      結帳去</router-link>
+                  </div>
+                </div>
               </div>
               <!-- 收藏夾 -->
               <div class="btn-group">
@@ -45,6 +92,41 @@
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      Carts: {},
+    };
+  },
+  methods: {
+    getCart() {
+      const vm = this;
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUTOMPATH}/cart`;
+      vm.$http.get(url).then((response) => {
+        if (response.data.success) {
+          vm.Carts = response.data.data.carts;
+        }
+      });
+    },
+    productLink(id) {
+      this.$router.push({ name: 'ProductDetail', params: { productId: id } });
+    },
+    removeCartItem(id) {
+      const vm = this;
+      const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUTOMPATH}/cart/${id}`;
+      this.$http.delete(url).then((response) => {
+        vm.getCart();
+        vm.$bus.$emit('message:push', response.data.message, 'warning');
+      });
+    },
+  },
+  created() {
+    this.getCart();
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .navbar .navbar-brand {
@@ -76,5 +158,47 @@
   top: auto;
   bottom: 18px;
   right: -5px;
+}
+
+.dropdown-menu {
+  position: absolute;
+  left: -248px;
+  width: 300px;
+  border-color: #e9eaec;
+  &:before {
+    position: absolute;
+    content: "";
+    top: -20px;
+    left: 92%;
+    transform: translateX(-50%);
+    border-width: 10px;
+    border-style: solid;
+    border-color: transparent transparent #e9eaec transparent;
+  }
+  &:after {
+    position: absolute;
+    content: "";
+    top: -18px;
+    left: 92%;
+    transform: translateX(-50%);
+    border-width: 10px;
+    border-style: solid;
+    border-color: transparent transparent #ffffff transparent;
+  }
+  .bg-cover {
+    width: 35px;
+    height: 35px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+  .btn-del {
+    &:hover {
+      margin-right: -4px;
+      i {
+        font-size: 1.25rem;
+      }
+    }
+  }
 }
 </style>
