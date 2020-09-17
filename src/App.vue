3e4a7e5569abb7998a9530b86fb8e4df5ad9c857
王佩馨 <lovv840629y@gmail.com>
2020-09-17 21:23:13 +0800

    refactor: 套用 滿版讀取效果 Vuex Module in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <Loading :active.sync="status.isLoading"></Loading>
     <router-view/>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['status']),
+  },
+};
+</script>
 
 <style lang="scss">
 @import "./assets/all.scss";
