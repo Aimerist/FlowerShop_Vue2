b5743c97859a78a91439670593b3b1ef022368f7
王佩馨 <lovv840629y@gmail.com>
2020-09-17 18:03:01 +0800

    style: 多處樣式調整 in 後台分頁|元件 1. 判斷 沒有多個分頁時就不顯示 2. 新增 上下頁 iocn圖 3. 判斷 當頁時的樣式

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,21 +1,23 @@
 <template>
-  <div>
-    <nav aria-label="Page navigation example">
+  <div v-if="page.total_pages !== 1">
+    <nav aria-label="Page navigation">
       <ul class="pagination justify-content-center">
         <li class="page-item" :class="{'disabled': !page.has_pre}">
           <a class="page-link" href="#" aria-label="Previous"
             @click.prevent="ChanePage(page.current_page - 1)">
-            <span aria-hidden="true">&laquo;</span>
+            <span aria-hidden="true"><i class="fas fa-arrow-left"></i></span>
+            <span class="sr-only">Previous</span>
           </a>
         </li>
-        <li class="page-item"
-          v-for="page in page.total_pages" :key="page">
+        <li class="page-item" v-for="item in page.total_pages" :key="item"
+          :class="{'active': item == page.current_page}">
           <a class="page-link" href="#"
-            @click.prevent="ChanePage(page)">{{ page }}</a></li>
+            @click.prevent="ChanePage(item)">{{ item }}</a></li>
         <li class="page-item"  :class=" {'disabled': !page.has_next}">
           <a class="page-link" href="#" aria-label="Next"
             @click.prevent="ChanePage(page.current_page + 1)">
-            <span aria-hidden="true">&raquo;</span>
+            <span aria-hidden="true"><i class="fas  fa-arrow-right"></i></span>
+            <span class="sr-only">Next</span>
           </a>
         </li>
       </ul>
