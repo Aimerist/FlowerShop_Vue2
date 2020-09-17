7cc4a9264574d8ba1e027263fd5e48dfcb10d980
王佩馨 <lovv840629y@gmail.com>
2020-09-17 17:59:31 +0800

    style: 多處樣式調整 in 訂單列表頁|後台 1. 頁面名稱 縮排、 顏色 2. 列表 table 排版、字體對齊

--- src/views/back/OrderList.vue
+++ src/views/back/OrderList.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap
       align-items-center pt-3 pb-2">
-      <h3 class="h3">訂單列表</h3>
+      <h2 class="ml-5 my-2 font-weight-bold text-primary">訂單列表</h2>
     </div>
     <table class="table table-hover text-left">
       <thead>
         <tr>
-          <th class="" scope="col-1">是否付款</th>
+          <th class="text-center" scope="col">是否付款</th>
           <th class="d-lg-table-cell d-none" scope="col">訂單編號</th>
           <th class="" scope="col">購買品項</th>
-          <th class="d-sm-table-cell d-none" scope="col">應付金額</th>
+          <th class="d-sm-table-cell d-none text-right" scope="col">應付金額</th>
           <th class="d-lg-table-cell d-none" scope="col">購買時間</th>
           <th class="d-xl-table-cell d-none" scope="col">留言</th>
           <th class="" scope="col">編輯</th>
@@ -18,7 +18,7 @@
       </thead>
       <tbody>
         <tr v-for="item in orders" :key="item.id">
-          <td scope="row" class="align-middle">
+          <td scope="row" class="align-middle text-center">
             <span v-if="item.is_paid" class="text-success">付款</span>
             <span v-else class="text-danger">未付款</span>
           </td>
@@ -28,7 +28,8 @@
               <li scope="row" class="list-unstyled">{{ product.product.title }}</li>
             </ul>
           </td>
-          <td scope="row" class="d-sm-table-cell d-none align-middle">{{item.total | currency}}</td>
+          <td scope="row" class="d-sm-table-cell d-none align-middle text-right">
+            {{item.total | currency}}</td>
           <td scope="row" class="d-lg-table-cell d-none align-middle">{{item.create_at | date}}</td>
           <td scope="row" class="d-xl-table-cell d-none align-middle">{{item.message}}</td>
           <td scope="row" class="align-middle">
